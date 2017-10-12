f74532fa956303d78461d736e7ed8cc121d37ac8
Samuel Mitas <s.mitas@devinprint.sk>
2017-10-12 16:10:33 +0200

    Make scip-4 shared

--- Formula/scip-4.rb
+++ Formula/scip-4.rb
@@ -5,15 +5,11 @@ class Scip4 < Formula
   sha256 "087535760eae3d633e2515d942a9b22e1f16332c022be8d093372bdc68e8a661"
   version "4.0.0"
 
-  depends_on "homebrew/science/ipopt"
-  depends_on "homebrew/homebrew-dupes/zlib"
-
-  depends_on "gmp"
-  depends_on "readline"
-
   def install
-    system "make", "ZIMPL=true", "SHARED=false"
-    system "make", "install", "ZIMPL=true", "SHARED=false", "INSTALLDIR=#{prefix}"
+    system "make", "all", "GMP=false", "SHARED=true", "READLINE=false", "ZLIB=false"
+    system "make", "test"
+    system "make", "install", "GMP=false", "SHARED=true", "READLINE=false", "ZLIB=false",
+        "INSTALLDIR=#{prefix}"
 
     bin.install
     lib.install
